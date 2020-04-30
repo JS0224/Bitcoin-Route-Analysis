cb47568f2e8b8060685ea037d2274f2d7ad87b0f
Jisoo Kim <sjw03151@naver.com>
2020-04-30 16:16:52 +0900

    Add socket module

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
-var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
-/*  fs.readFile('index.html', function (err,data){
-    if(err){
-      console.log(err)
-    }else{
-      res.writeHead(200, {'Content-Type' : 'text/html'});
-      res.end(data)
-    }
-  });*/
-  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
