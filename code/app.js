76aa679391526173134c88f0c4f80aee002382c5
Jisoo Kim <sjw03151@naver.com>
2020-04-30 16:00:07 +0900

    Render html with ejs engine

--- code/app.js
+++ code/app.js
@@ -3,15 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +39,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  fs.readFile('ErrorPage.html', function(err,data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
 });
 
 module.exports = app;
--- code/routes/index.js
+++ code/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile('index.html', function (err,data){
-
-  });
+  res.render('index.html');
+/*  fs.readFile('index.html', function (err,data){
+    if(err){
+      console.log(err)
+    }else{
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.end(data)
+    }
+  });*/
   //res.render('index', { title: 'Express' });
 });
 
