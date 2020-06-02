71d55f55f869f7f0ef1d0dfdba4089febea67b0f
Jisoo Kim <sjw03151@naver.com>
2020-06-02 15:11:52 +0900

    tab1 complete

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+//const mariaDB = require('../mariaDB');
+//const dbConn = mariaDB.getConnection();
+//const Client = require('bitcoin-core');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
+  /*let sql ='SELECT id from Blocks where id < 10';
+  dbConn.query(sql, function (error, results, fields) {
+    if (error) throw error;
+      //console.log(results);
+  });*/
+  //let rows = mariaDB.getDataFromDB(dbConn,sql);
+  //console.log("rows:",rows);
 });
 
 module.exports = router;
